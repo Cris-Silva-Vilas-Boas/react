eff6c424d5dee5529fa71ac911136f825a2d6885
Cris-Silva-Vilas-Boas <cris.silva.vilas.boas.developer@gmail.com>
2021-12-14 23:51:13 -0300

    Correção

--- src/routes.js
+++ src/routes.js
@@ -5,18 +5,18 @@ import Home from './pages/Home';
 import Dragon from './pages/Dragon';
 import Cadastro from './pages/Cadastro';
 import Edicao from './pages/Edicao';
-import Login  from './pages/login';
+import Login  from './pages/Login';
 
 const Rotas = () => {
   return(
     <BrowserRouter>
       <Header/>
       <Routes>
-          <Route path="/" element={<Home />}></Route>
+          <Route path="/home" element={<Home />}></Route>
           <Route path="/dragon/:id" element={<Dragon />}></Route>
           <Route path="/api/v1/dragon" element={<Cadastro />}></Route>
-          <Route path="edit/dragon/:id" element={<Edicao />}></Route>
-          <Route path="/login" element={<Login />}></Route>
+          <Route path="/edit/dragon/:id" element={<Edicao />}></Route>
+          <Route path="/" element={<Login />}></Route>
       </Routes>
     </BrowserRouter>
   )
